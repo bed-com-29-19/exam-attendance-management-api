089f3e6ef981229918273b05b0b0910680e29543
bed-com-29-19 <bed-com-29-19@unima.ac.mw>
2023-12-09 23:41:40 +0200

    resource actors added

--- src/main.ts
+++ src/main.ts
@@ -4,16 +4,31 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const config = new DocumentBuilder()
-    .setTitle('UEAMS')
-    .setDescription('UNIMA EXAM ATTENDANCE MANAGEMENT SYSTEM')
+
+  // Swagger configuration
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('UEAMS API Documentation')
+    .setDescription(
+      'API documentation for UNIMA EXAM ATTENDANCE MANAGEMENT SYSTEM',
+    )
     .setVersion('1.0')
-    .addTag('administrator')
-    .addTag('invigilator')
+    .addTag('administrator', 'Operations related to administrators')
+    .addTag('invigilator', 'Operations related to invigilators')
+    .setBasePath('api') // Set a base path for your API, e.g., '/api'
+    .addBearerAuth() // Enable authentication using Bearer token
     .build();
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+
+  // Create Swagger document
+  const swaggerDocument = SwaggerModule.createDocument(app, swaggerConfig);
+
+  // Setup Swagger UI with custom theme
+  SwaggerModule.setup('api', app, swaggerDocument, {
+    swaggerOptions: {
+      theme: 'muted', // Use the 'muted' theme from swagger-ui-themes
+    },
+  });
 
   await app.listen(3000);
 }
+
 bootstrap();
